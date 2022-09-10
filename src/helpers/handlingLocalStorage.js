33635e05a7b4840dff377c126aaac918289ab460
Brenno Lucas <104790785+Brenno-Lucas@users.noreply.github.com>
2022-09-09 22:22:03 -0300

    Removendo espaço extra

--- src/helpers/handlingLocalStorage.js
+++ src/helpers/handlingLocalStorage.js
@@ -2,6 +2,6 @@ export const setTokenStorage = (string) => {
   localStorage.setItem('token', string);
 };
 
-export  const getTokenStorage = () => localStorage.getItem('token');
+export const getTokenStorage = () => localStorage.getItem('token');
 
 export default setTokenStorage;
