8c8314c13d7dfeaa09b37bdef80f5f4d4a817e4b
Brenno-Lucas <brennolucas8@gmail.com>
2022-09-10 11:17:15 -0300

    Requisito 07 - Completo

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -10,6 +10,7 @@ class Game extends React.Component {
     questions: [],
     indexQuestions: 0,
     answers: [],
+    // points: 0,
   };
 
   async componentDidMount() {
@@ -47,7 +48,11 @@ class Game extends React.Component {
       <button
         key={ 3 }
         type="button"
+        name={ [questions[indexQuestions].correct_answer] }
+        id={ [questions[indexQuestions].correct_answer] }
+        value={ [questions[indexQuestions].correct_answer] }
         data-testid="correct-answer"
+        onClick={ this.onSubmitAnswer }
       >
         {[questions[indexQuestions].correct_answer]}
       </button>)];
@@ -56,7 +61,11 @@ class Game extends React.Component {
         <button
           key={ i }
           type="button"
+          name={ element }
+          id={ element }
+          value={ element }
           data-testid={ `wrong-answer-${i}` }
+          onClick={ this.onSubmitAnswer }
         >
           {element}
         </button>));
@@ -66,6 +75,30 @@ class Game extends React.Component {
     });
   };
 
+  onSubmitAnswer = () => {
+    const { questions, indexQuestions } = this.state;
+    const correctAnswer = questions[indexQuestions].correct_answer;
+    const answers = document.getElementById('answer-options');
+    const answersElements = [];
+    Object.values(answers.children)
+      .forEach((item) => answersElements.push(item));
+    const correctButton = answersElements
+      .filter((item) => item.value === correctAnswer)[0].textContent;
+    document.getElementById(correctButton)
+      .setAttribute('style', 'border: 3px solid rgb(6, 240, 15)');
+    const incorrectButton = answersElements.filter((item) => item.value !== correctAnswer)
+      .map((i) => i.textContent);
+    incorrectButton.map((i) => document.getElementById(i)
+      .setAttribute('style', 'border: 3px solid red'));
+    // Esse trecho se refere a um somador de pontos e o indexQuestion(para ir avançando nas questões)
+    // this.setState({
+    // if (value === correctAnswer) {
+    //     points: points + 1,
+    //     indexQuestions: indexQuestions + 1,
+    //   }, () => { this.getCaptureAnswers(); });
+    // }
+  };
+
   render() {
     const { answers, questions, indexQuestions } = this.state;
     const NUMBERMAGICBODY = 4;
@@ -73,7 +106,7 @@ class Game extends React.Component {
       <div>
         <Header />
         <div>Game</div>
-        {questions[indexQuestions] && ( // iniciando requisito 07
+        {questions[indexQuestions] && (
           <div>
             <p
               data-testid="question-category"
@@ -85,7 +118,7 @@ class Game extends React.Component {
             >
               { questions[indexQuestions].question }
             </p>
-            <div data-testid="answer-options">
+            <div data-testid="answer-options" id="answer-options">
               {answers.slice(0, NUMBERMAGICBODY)}
             </div>
           </div>
